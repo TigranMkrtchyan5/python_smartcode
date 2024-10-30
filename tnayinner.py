969f0487bf34eb38088e932099c817d1490806be
TigranMkrtchyan5 <123tigranmkrtchyan08@gmail.com>
2024-10-30 22:21:04 +0400

    version1

--- tnayinner.py
+++ tnayinner.py
@@ -1,3 +1,4 @@
+# N173
 # def func():
 #     count = 0
 #     while True:
@@ -9,5 +10,48 @@
 #             print(count)
 
 # func()
-
-print(5)
+# -----------------------------------------------------------------------------
+# N174
+# def alo(a, b):
+#     if b == 0:
+#         return a
+#     else:
+#         return alo(b, a % b)
+# print(alo(12, 15))
+# --------------------------------------------------------------------------------------
+# N176
+def myfunc():
+    nato_alphabet = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu"
+}
+    text = input('greq texty: ')
+    if text == '':
+        return '$'
+    else:
+        text.split('')[0:]
+        return 
